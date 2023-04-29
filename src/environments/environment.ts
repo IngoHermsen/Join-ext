ff715ccc4b47cc0e225dd3a9fe803b3ccabb2ad9
Ingo <ingohermsen@gmail.com>
2023-04-29 12:48:33 +0200

    connected firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'join-extended',
+    appId: '1:909469207063:web:8b6c71f1fc3ecf4f23aa52',
+    storageBucket: 'join-extended.appspot.com',
+    apiKey: 'AIzaSyDFZgg2vHV--UPaCUx6bbM4JdAWI2MYTHs',
+    authDomain: 'join-extended.firebaseapp.com',
+    messagingSenderId: '909469207063',
+  },};
